ecf0f358fd3819b06531340184dbf11fa41995d3
tanuki-er <hagamura@mail.ru>
2021-12-20 17:36:17 +0300

    feat: Algorithms are added

---
+++ BackupsExtra/Algorithms/Algorithms.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using BackupsExtra.Service;
+
+namespace BackupsExtra.Algorithms
+{
+    public abstract class Algorithms
+    {
+        public RestorePoint CreateRestorePoint(DirectoryInfo directoryInfo)
+        {
+            var restorePoint = new RestorePoint();
+            restorePoint.CreatingTime = DateTime.Now;
+            foreach (string variable in Directory.GetFiles(directoryInfo.FullName))
+            {
+                restorePoint.RestorePointDictionary.Add(new NewStorage(variable), new NewFileClass(variable));
+            }
+
+            return restorePoint;
+        }
+
+        public abstract List<RestorePoint> CleanRestorePoints(List<RestorePoint> restorePoints, int counter, DateTime time);
+    }
+}
---
+++ BackupsExtra/Algorithms/CleaningByDate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using BackupsExtra.Service;
+using RestorePoint = BackupsExtra.Service.RestorePoint;
+
+namespace BackupsExtra.Algorithms
+{
+    public class CleaningByDate : Algorithms
+    {
+        public override List<Service.RestorePoint> CleanRestorePoints(List<Service.RestorePoint> restorePoints, int counter, DateTime time)
+        {
+            NewStorage newStorage = null;
+            foreach (RestorePoint variable in restorePoints)
+            {
+                foreach ((NewStorage key, NewFileClass value) in variable.RestorePointDictionary)
+                {
+                    if (key.DateCreateTime < time) newStorage = key;
+                    variable.RestorePointDictionary.Remove(newStorage!);
+                }
+            }
+
+            return restorePoints;
+        }
+    }
+}
