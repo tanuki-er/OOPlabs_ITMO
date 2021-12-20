b7fe7e39c62775af74448221c3f5a2efe53b54c2
tanuki-er <hagamura@mail.ru>
2021-12-20 17:36:51 +0300

    feat: Service data were added

---
+++ BackupsExtra/Service/NewStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BackupsExtra.Service
+{
+    public class NewStorage
+    {
+        public NewStorage(string fileWay)
+        {
+            FilePath = fileWay;
+        }
+
+        public DateTime DateCreateTime { get => DateTime; set => DateTime = value; }
+        public string Path { get => FilePath; set => FilePath = value; }
+        private DateTime DateTime { get; set; } = DateTime.Now;
+        private string FilePath { get; set; }
+    }
+}
