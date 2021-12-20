c3d6a017ea9c7260fd7a841986bb7b862af5bef0
tanuki-er <hagamura@mail.ru>
2021-12-20 17:37:26 +0300

    feat: backupJob was added

---
+++ BackupsExtra.Tests/BackupsExtraTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Backups.Types.Service;
+using BackupsExtra.Algorithms;
+using BackupsExtra.Service;
+using NUnit.Framework;
+
+namespace BackupsExtra.Tests
+{
+    public class BackupsExtraTests
+    {
+        private NewBackupJob _backupJob;
+        private FileArchiveSystem _fileArchiveSystem;
+        private DirectoryInfo _directoryInfo = new DirectoryInfo(@"C:\Users\hagam\RiderProjects\Extra");
+
+        [SetUp]
+        public void Setup()
+        {
+            _directoryInfo.Create();
+            _backupJob = new NewBackupJob();
+            _fileArchiveSystem = new FileArchiveSystem();
+            var fileA = new FileInfo("1.txt");
+            fileA.Create();
+            var fileB = new FileInfo("2.txt");
+            fileB.Create();
+            RestorePoint point1 = _backupJob.CreateRestorePoint(new CleaningByDate(), _directoryInfo);
+            fileA.MoveTo(_directoryInfo.FullName);
+            RestorePoint point2 = _backupJob.CreateRestorePoint(new CleaningByCounter(), _directoryInfo);
+            fileB.MoveTo(_directoryInfo.FullName);
+            RestorePoint point3 = _backupJob.CreateRestorePoint(new CleaningByCounter(), _directoryInfo);
+            /*___*/
+            _backupJob.MergeRestorePoints(_directoryInfo, point2, point3);
+            _backupJob.CleanRestorePoint(new CleaningByCounter(), 1, DateTime.Now);
+        }
+    }
+}
---
+++ BackupsExtra/Service/NewBackupJob.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using BackupsExtra.Algorithms;
+
+namespace BackupsExtra.Service
+{
+    public class NewBackupJob
+    {
+        private List<RestorePoint> RestorePoints { get; set; } = new List<RestorePoint>();
+
+        public void LogFile(DirectoryInfo directoryInfo, string message)
+        {
+            var fileStream = new FileStream(directoryInfo + "\\logs.log", FileMode.OpenOrCreate);
+            byte[] array = System.Text.Encoding.Default.GetBytes(message);
+            fileStream.Write(array, 0, array.Length);
+            fileStream.Close();
+        }
+
+        public RestorePoint CreateRestorePoint(Algorithms.Algorithms algorithms, DirectoryInfo directoryInfo)
+        {
+            RestorePoint rPoint = algorithms.CreateRestorePoint(directoryInfo);
+            RestorePoints.Add(rPoint);
+            return rPoint;
+        }
+
+        public void CleanRestorePoint(Algorithms.Algorithms algorithms, int counter, DateTime time)
+        {
+            algorithms.CleanRestorePoints(RestorePoints, counter, time);
+        }
+
+        public void MergeRestorePoints(DirectoryInfo directoryInfo, RestorePoint restorePoint, RestorePoint secondRestorePoint)
+        {
+            foreach (KeyValuePair<NewStorage, NewFileClass> oldValues in restorePoint.RestorePointDictionary)
+            {
+                foreach ((NewStorage key, NewFileClass value) in secondRestorePoint.RestorePointDictionary)
+                {
+                    if (oldValues.Key.DateCreateTime < key.DateCreateTime && oldValues.Value.FileInfo == value.FileInfo)
+                    {
+                        oldValues.Key.DateCreateTime = key.DateCreateTime;
+                        value.FileInfo.MoveTo(directoryInfo.FullName);
+                    }
+
+                    if (!value.FileInfo.Equals(oldValues.Value.FileInfo)) oldValues.Value.FileInfo.MoveTo(directoryInfo.FullName);
+                }
+            }
+        }
+
+        public void GetFileTree(DirectoryInfo directoryInfo)
+        {
+            Algorithms.Algorithms algorithms = new CleaningByCounter();
+            string[] allFiles = Directory.GetFiles(directoryInfo.FullName);
+            foreach (string filename in allFiles)
+            {
+                RestorePoints.Add(CreateRestorePoint(algorithms, directoryInfo));
+                Console.WriteLine(filename);
+            }
+        }
+
+        public void RecoveryFromRestorePoint(DirectoryInfo directoryInfo, RestorePoint restorePoint)
+        {
+            foreach (RestorePoint variable in RestorePoints)
+            {
+                if (variable != restorePoint) continue;
+                directoryInfo.Delete();
+                directoryInfo.Create();
+                foreach ((NewStorage key, NewFileClass value) in variable.RestorePointDictionary)
+                {
+                    value.FileInfo.Create();
+                    value.FileInfo.MoveTo(directoryInfo.FullName);
+                }
+            }
+        }
+    }
+}
